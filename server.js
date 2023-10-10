cd0b58eef4f968d5feca8e1c2852e059bbfa03fa
Zakaria Bennane <zakariamonre@gmail.com>
2023-10-10 22:42:44 +0300

    finished

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-import mongoose from 'mongoose';
-import UserModel from './UserModel';
-import usersList from '';
+import mongoose, { connect } from 'mongoose';
+import UserModel from './UserModel.js';
+import usersList from './usersList.js';
+import jwt from 'jsonwebtoken';
 
 
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://Zak_Ben:HitOsQDcsfEWmypK@serverlessinstance.ppe7yuq.mongodb.net/?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        console.log('Connected to MongoDB');
+      await connect('mongodb+srv://Zak_Ben:4Zvzt6zNLcz20Dqx@serverlessinstance.ppe7yuq.mongodb.net/?retryWrites=true&w=majority');
+      console.log('Connected to MongoDB');
     } catch (err) {
-        console.error('Error connecting to MongoDB', err);
-        process.exit(1); // Exit process with failure
+      console.error('Error connecting to MongoDB', err);
+      process.exit(1); // Exit process with failure
     }
 };
 
@@ -39,6 +36,16 @@ const createUsers = async (users) => {
 
             const savedUser = await newUser.save();
             console.log('User added successfully:', savedUser._id);
+
+            // Generating the JWT token
+            const tokenPayload = {
+              userId: savedUser._id,
+              platform: 'pinterest'
+            };
+
+            const expiresIn = 50 * 60 * 60; // 50 hours in seconds
+            const token = jwt.sign(tokenPayload, '876km1qjDUJRQ09wHGCX2pnezvrLY5s', { expiresIn });
+            console.log('Generated JWT Token:', token);
             
         } catch (err) {
             console.error('Error adding user', err.message);
